ea90e56a534e9263731a34484739f2e499e27856
mhebd <mhemonhossain@gmail.com>
2025-02-13 08:25:06 +0600

    tailwind screen added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,12 @@ export default {
 	darkMode: 'class',
 	theme: {
 		extend: {
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+			},
 			colors: {
 				background: 'var(--background)',
 				foreground: 'var(--foreground)',
